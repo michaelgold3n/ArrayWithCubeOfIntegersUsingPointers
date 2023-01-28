d1431e4f8a76abc46c5d9c825fb678e38e1eb9f0
Michael Golden <67241067+michaelgold3n@users.noreply.github.com>
2023-01-27 20:03:13 -0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,42 @@
+/*HW Assignment_B.Green.cpp
+Bryce Green
+4/11/2021
+Version 1.5
+The program uses a pointer to dynamically allocate the size of a array that is given by the user and also allows input and cubes the outputted values*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int* makingthearray(int size) //function that creates an array and also creates a pointer that is assigned to the array. The function calls for an integer from the main function called size
+{
+	int* array = new int[size];
+
+	return array;
+}
+
+int main()
+{
+	int value = 0; //creates a variable called value with datatype integer
+	
+	cout << "How many numbers do you want to enter? "; //asks user for the size they want the array to be
+	cin >> value; //input a number for value
+	int* array = makingthearray(value); //assigns a pointer to the array from the makingthearray function by using the value variable and the number stored from the user
+
+
+	for (int i = 0; i < value; i++) //for loop to input each value of the dynamic array
+	{
+		cout << "Enter an integer number: ";
+		cin >> array[i];
+	}
+
+	cout << "Here are the cube of the values you entered:\n"; //outputs the cubed values that were just inputted by the user
+	for (int i = 0; i < value; i++)
+	{
+		cout << pow(array[i],3) << endl;
+	}
+	
+	delete[] array; //clears the dynamically allocated memory
+	return 0;
+}
+
